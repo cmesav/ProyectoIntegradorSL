653c09f22e3a61e405a1422f29c28d82a2f9191e
elkinparo <elkin.palacios.qa@gmail.com>
2025-04-14 14:34:03 -0500

    conexion a la DB

---
+++ Utilidades/configuracion.py
@@ -0,0 +1,8 @@
+class Configuracion:
+         str_connection = """
+        Driver={MySQL ODBC 9.2 Unicode Driver};
+        Server=localhost;
+        Database=SistemaTransacciones;
+        PORT=3306;
+        user=root;
+    """
